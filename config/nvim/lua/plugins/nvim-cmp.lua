4e340a977d31876aa8968f04e0562dea7ddad034
Eitan Ostashinski <eitanoid.os@gmail.com>
2025-03-24 17:41:17 +0000

    modified CMP with more personal configs, updated cmp loading time, updated latex settings to give better linting and completion.

--- config/nvim/lua/plugins/nvim-cmp.lua
+++ config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,87 +1,122 @@
 return function()
-	-- See `:help cmp`
-	local cmp = require("cmp")
-	local luasnip = require("luasnip")
-	luasnip.config.setup({})
+    -- See `:help cmp`
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    luasnip.config.setup({})
 
-	cmp.setup({
-		-- snippet = {
-		-- 	expand = function(args)
-		-- 		luasnip.lsp_expand(args.body)
-		-- 	end,
-		-- },
-		completion = { completeopt = "menu,menuone,noinsert" },
+    -- cmp.setup({})
+    -- snippet = {
+    -- 	expand = function(args)
+    -- 		luasnip.lsp_expand(args.body)
+    -- 	end,
+    -- },
+    local config = {
+        preselect = cmp.PreselectMode.None,
+        completion = {
+            completeopt = "menu,menuone,noinsert",
+            keyword_length = 0,
+            max_item_count = 20,
+        },
 
-		-- For an understanding of why these mappings were
-		-- chosen, you will need to read `:help ins-completion`
-		--
-		-- No, but seriously. Please read `:help ins-completion`, it is really good!
-		mapping = cmp.mapping.preset.insert({
-			-- Select the [n]ext item
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<Tab>"] = cmp.mapping.select_next_item(),
-			-- Select the [p]revious item
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<S-Tab>"] = cmp.mapping.select_prev_item(),
-			["<C-S-n>"] = cmp.mapping.select_prev_item(),
-			["<CR>"] = cmp.mapping.confirm({ select = true }),
-			-- Scroll the documentation window [b]ack / [f]orward
-			-- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			-- ["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<C-Space>"] = cmp.mapping.complete({}),
+        window = {
+            completion = {
+                -- winblend = 70,
+                scrollbar = false,
+                border = "rounded", -- { "╔", "═", "╗", "║", "╝", "═", "╚", "║" }, -- see :h nvim_open_win
+                -- winhighlight = "Normal:Pmenu,CursorLine:CmpCursorLine,Search:None",
+                col_offset = -3,
+                side_padding = 0,
+            },
+            documentation = cmp.config.window.bordered(),
+        },
 
-			-- Think of <c-l> as moving to the right of your snippet expansion.
-			--  So if you have a snippet that's like:
-			--  function $name($args)
-			--    $body
-			--  end
-			--
-			-- <c-l> will move you to the right of each of the expansion locations.
-			-- <c-h> is similar, except moving you backwards.
-			["<C-l>"] = cmp.mapping(function()
-				if luasnip.expand_or_locally_jumpable() then
-					luasnip.expand_or_jump()
-				end
-			end, { "i", "s" }),
-			["<C-h>"] = cmp.mapping(function()
-				if luasnip.locally_jumpable(-1) then
-					luasnip.jump(-1)
-				end
-			end, { "i", "s" }),
+        experimental = {
+            ghost_text = false, -- this feature conflict with copilot.vim's preview.
+        },
 
-			-- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
-			--    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
-		}),
-		sources = {
-			{
-				name = "lazydev",
-				-- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
-				group_index = 0,
-			},
-			{ name = "git" },
-			{ name = "nvim_lsp" },
-			{ name = "omni" },
-			{ name = "luasnip" },
-			{ name = "path" }, -- file system path
-			{ name = "go_pkgs" },
-		},
-	})
+        -- For an understanding of why these mappings were
+        -- chosen, you will need to read `:help ins-completion`
+        --
+        -- No, but seriously. Please read `:help ins-completion`, it is really good!
+        mapping = cmp.mapping.preset.insert({
+            -- Select the [n]ext item
+            ["<C-n>"] = cmp.mapping.select_next_item(),
+            ["<Tab>"] = cmp.mapping.select_next_item(),
+            -- Select the [p]revious item
+            ["<C-p>"] = cmp.mapping.select_prev_item(),
+            ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+            ["<C-S-n>"] = cmp.mapping.select_prev_item(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            -- Scroll the documentation window [b]ack / [f]orward
+            -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete({}),
 
-	cmp.setup.filetype({ "tex", "bib" }, { --TODO: FINISH THIS.
-		sources = cmp.config.sources({
-			{ name = "git" },
-			{ name = "nvim_lsp" },
-			{ name = "omni" },
-			{
-				name = "latex_symbols",
-				option = {
-					strategy = 2,
-				},
-			},
-			{
-				name = "vimtex",
-			},
-			{ name = "path" }, -- file system path
-		}),
-	})
+            -- Think of <c-l> as moving to the right of your snippet expansion.
+            --  So if you have a snippet that's like:
+            --  function $name($args)
+            --    $body
+            --  end
+            --
+            -- <c-l> will move you to the right of each of the expansion locations.
+            -- <c-h> is similar, except moving you backwards.
+            ["<C-l>"] = cmp.mapping(function()
+                if luasnip.expand_or_locally_jumpable() then
+                    luasnip.expand_or_jump()
+                end
+            end, { "i", "s" }),
+            ["<C-h>"] = cmp.mapping(function()
+                if luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
+                end
+            end, { "i", "s" }),
+
+            -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
+            --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
+        }),
+    }
+
+    config.sources = {
+        {
+            name = "lazydev",
+            -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
+            group_index = 0,
+        },
+        { name = "git" },
+        { name = "nvim_lsp" },
+        { name = "omni" },
+        { name = "luasnip" },
+        { name = "path" }, -- file system path
+    }
+
+    config.formatting = {
+        fields = { "menu", "abbr", "kind" },
+        format = function(entry, item)
+            item.menu = entry.source.name
+            return item
+        end,
+    }
+
+    cmp.setup(config)
+
+    cmp.setup.filetype({ "tex", "bib" }, { --TODO: FINISH THIS.
+        sources = cmp.config.sources({
+            { name = "git" },
+            { name = "nvim_lsp", group_index = 2, max_item_count = 7 },
+            { name = "omni" },
+            -- { name = "latex_symbols", max_item_count = 8, group_index = 2, option = { strategy = 2 } },
+            { name = "vimtex", group_index = 1, max_item_count = 8 },
+            { name = "path" }, -- file system path
+        }),
+    })
+
+    cmp.setup.filetype({ "go" }, { --TODO: FINISH THIS.
+        sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "go_pkgs" },
+            { name = "git" },
+            { name = "omni" },
+            { name = "path" }, -- file system path
+        }),
+    })
 end
--- config/nvim/lua/plugins/vimtex.lua
+++ config/nvim/lua/plugins/vimtex.lua
@@ -1,22 +1,23 @@
 return function()
-	-- VimTeX configuration goes here, e.g.
-	vim.g.vimtex_view_method = "zathura"
-	vim.g.vimtext_compiler_method = "latexmk"
-	vim.g.vimtex_complier_latexmk_engine = {
-		_ = "-xelatex", -- set default engine and it's not doing anything.
-	}
-	vim.g.vimtex_compiler_latexmk = {
-		aux_dir = "./tex/aux",
-		out_dir = "./tex/out",
-		callback = 1,
-		continuous = 1,
-		executable = "latexmk",
-		-- hooks = {},
-		options = {
-			"-verbose",
-			"-file-line-error",
-			"-synctex=1",
-			"-interaction=nonstopmode",
-		},
-	}
+    -- VimTeX configuration goes here, e.g.
+    vim.g.vimtex_view_method = "zathura"
+    vim.g.tex_flavor = "latex"
+    vim.g.vimtext_compiler_method = "latexmk"
+    vim.g.vimtex_complier_latexmk_engine = {
+        _ = "-xelatex", -- set default engine and it's not doing anything.
+    }
+    vim.g.vimtex_compiler_latexmk = {
+        aux_dir = "./tex/aux",
+        out_dir = "./tex/out",
+        callback = 1,
+        continuous = 1,
+        executable = "latexmk",
+        -- hooks = {},
+        options = {
+            "-verbose",
+            "-file-line-error",
+            "-synctex=1",
+            "-interaction=nonstopmode",
+        },
+    }
 end
