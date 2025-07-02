d8fea69f7275569e52e3b13108d50a8d56fd46c4
Eitan Ostashinski <eitanoid.os@gmail.com>
2025-07-02 11:44:48 +0100

    changed formatter so added spacing in treesitter.lua added haskell language server changed one button in alpha greeter

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -169,6 +169,8 @@ return function()
 
         rust_analyzer = {},
 
+        hls = {}, -- haskell language server
+
         bashls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,35 +1,36 @@
 local M = {}
 
 M.opts = {
-	ignore_install = { "latex" },
-	ensure_installed = {
-		"printf",
-		"bash",
-		"c",
-		"diff",
-		"html",
-		"lua",
-		"luadoc",
-		"markdown",
-		"markdown_inline",
-		"query",
-		"vim",
-		"vimdoc",
-		"go",
-		"python",
-	},
+    ignore_install = { "latex" },
+    ensure_installed = {
+        "printf",
+        "bash",
+        "c",
+        "diff",
+        "html",
+        "lua",
+        "luadoc",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "vim",
+        "vimdoc",
+        "go",
+        "python",
+        "gap",
+    },
 
-	-- Autoinstall languages that are not installed
-	auto_install = true,
-	highlight = {
-		enable = true,
-		-- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-		--  If you are experiencing weird indenting issues, add the language to
-		--  the list of additional_vim_regex_highlighting and disabled languages for indent.
-		disable = { "latex" },
-		additional_vim_regex_highlighting = { "ruby", "markdown" },
-	},
-	indent = { enable = true, disable = { "ruby" } },
+    -- Autoinstall languages that are not installed
+    auto_install = true,
+    highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        disable = { "latex" },
+        additional_vim_regex_highlighting = { "ruby", "markdown" },
+    },
+    indent = { enable = true, disable = { "ruby" } },
 }
 
 return M
