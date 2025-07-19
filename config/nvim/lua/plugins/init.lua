8618479444755bc88c5784ee24867edc9cd778ba
Eitan Ostashinski <eitanoid.os@gmail.com>
2025-07-19 16:14:27 +0100

    vim tmux navigator works now, updatedtmuxfile updated deploy script

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -667,10 +667,29 @@ require("lazy").setup({
         end,
     },
 
-    { -- vim and tmux compabibility
+    -- { -- vim and tmux compabibility
+    --     "christoomey/vim-tmux-navigator",
+    --     cmd = require("plugins.vim-tmux-navigator").cmd,
+    --     keys = require("plugins.vim-tmux-navigator").keys,
+    -- },
+
+    {
         "christoomey/vim-tmux-navigator",
-        cmd = require("plugins.vim-tmux-navigator").cmd,
-        keys = require("plugins.vim-tmux-navigator").keys,
+        cmd = {
+            "TmuxNavigateLeft",
+            "TmuxNavigateDown",
+            "TmuxNavigateUp",
+            "TmuxNavigateRight",
+            "TmuxNavigatePrevious",
+            "TmuxNavigatorProcessList",
+        },
+        keys = {
+            { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+            { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+            { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+            { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+            { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+        },
     },
 
     ------------------
